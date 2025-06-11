82b905b814d4ed803609d92a48935682a392b130
jcgcastolo <jcgcastolo@cucea.udg.mx>
2025-06-11 15:53:43 -0600

    Se eliminaron línea que no servian y se agregaron otras que son mejores

--- CTarea0cod.R
+++ CTarea0cod.R
@@ -1,9 +1,14 @@
+# Revisar:
 # rmarkdown
+
+# Video para vincular GitHup con R
 # https://www.youtube.com/watch?v=BAN4ErGF5ro
+
+# Lineas de código que se deben correr en la consola de R
 # usethis::create_github_token()
 # gitcreds::gitcreds_set()
+
+# Token generado del espacio GipHup para ser usado en otra compu
 # token:  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
 
-library(ggplot2) ssss
 
-hhhhhhhhhhhhh
